4de2b8144934eaa281eceb651b59e70069efa5e5
Andrey Nazarov <skuller@skuller.net>
2022-08-19 20:26:07 +0300

    Add support for loading binary ‘.override’ files.
    Also treat ‘map_override_path’ as directory name rather than arbitary
    prefix.

--- src/server/server.h
+++ src/server/server.h
@@ -171,7 +171,6 @@ typedef struct {
 
     char        name[MAX_QPATH];            // map name, or cinematic name
     cm_t        cm;
-    char        *entitystring;
 
     char        configstrings[MAX_CONFIGSTRINGS][MAX_QPATH];
 
@@ -565,8 +564,6 @@ extern cvar_t       *sv_uptime;
 extern cvar_t       *sv_allow_unconnected_cmds;
 
 extern cvar_t       *g_features;
-
-extern cvar_t       *map_override_path;
 
 extern cvar_t       *sv_timeout;
 extern cvar_t       *sv_zombietime;
