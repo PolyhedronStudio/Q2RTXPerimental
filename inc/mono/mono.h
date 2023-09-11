e6202ca36b9e52a418cd7f932bae652eac4eff85
WatIsDeze <10252874+WatIsDeze@users.noreply.github.com>
2023-09-11 20:15:01 +0200

    We can find, include, and link to mono.

---
+++ inc/mono/mono.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <mono/jit/jit.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/mono-debug.h>
+#include <mono/metadata/threads.h>
+#include <mono/utils/mono-logger.h>
+#include "mono_config.h"
