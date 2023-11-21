57b8035b7ca02b9603d017562d518f72bbd9b1c7
WatIsDeze <10252874+WatIsDeze@users.noreply.github.com>
2023-11-21 21:35:57 +0100

    Patched Cmd_PlayerList_f to use the appropriate sprintf types for int64_t.

--- src/baseq2/svgame/g_cmds.cpp
+++ src/baseq2/svgame/g_cmds.cpp
@@ -848,7 +848,7 @@ void Cmd_PlayerList_f(edict_t *ent)
         if (!e2->inuse)
             continue;
 
-        Q_snprintf(st, sizeof(st), "%02d:%02d %4d %3d %s%s\n",
+        Q_snprintf(st, sizeof(st), "%02ld:%02ld %4d %3d %s%s\n",
                    (level.framenum - e2->client->resp.enterframe) / 600,
                    ((level.framenum - e2->client->resp.enterframe) % 600) / 10,
                    e2->client->ping,
