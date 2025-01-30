85207de07143dc755037de2f823b2355187ebc05
WatIsDeze <10252874+WatIsDeze@users.noreply.github.com>
2025-01-30 04:06:26 +0100

    [QMath]: Added a bunch of easing functions.

--- inc/shared/math/qm_utils.h
+++ inc/shared/math/qm_utils.h
@@ -210,3 +210,290 @@ RMAPI const float QM_Angle_Normalize180( const float angle ) {
 RMAPI const float QM_AngleDelta( const float angle1, const float angle2 ) {
     return QM_Angle_Normalize180( angle1 - angle2 );
 }
+
+
+
+/**
+*
+*
+*
+*   Easing Utilities:
+*	- Source of Original: https://raw.githubusercontent.com/warrenm/AHEasing/refs/heads/master/AHEasing/easing.c
+*
+*
+**/
+#define USE_DOUBLE_PRECISION_EASING
+#ifdef USE_DOUBLE_PRECISION_EASING
+#define EASING_FLOAT double
+#else
+#define EASING_FLOAT float
+#endif
+/**
+*	@brief	Modeled after the line y = x
+**/
+static inline const EASING_FLOAT QM_LinearInterpolation( EASING_FLOAT p ) {
+	return p;
+}
+/**
+*	@brief	Modeled after the parabola y = x^2
+**/
+static inline const EASING_FLOAT QM_QuadraticEaseIn( EASING_FLOAT p ) {
+	return p * p;
+}
+/**
+*	@brief	Modeled after the parabola y = -x^2 + 2x
+**/
+static inline const EASING_FLOAT QM_QuadraticEaseOut( EASING_FLOAT p ) {
+	return -( p * ( p - 2 ) );
+}
+/**
+*	@brief	Modeled after the piecewise quadratic
+*			y = (1/2)((2x)^2)             ; [0, 0.5)
+*			y = -(1/2)((2x-1)*(2x-3) - 1) ; [0.5, 1]
+**/
+static inline const EASING_FLOAT QM_QuadraticEaseInOut( EASING_FLOAT p ) {
+	if ( p < 0.5 ) {
+		return 2 * p * p;
+	} else {
+		return ( -2 * p * p ) + ( 4 * p ) - 1;
+	}
+}
+
+/**
+*	@brief	Modeled after the cubic y = x^3
+**/
+static inline const EASING_FLOAT QM_CubicEaseIn( EASING_FLOAT p ) {
+	return p * p * p;
+}
+/**
+*	@brief	Modeled after the cubic y = (x - 1)^3 + 1
+**/
+static inline const EASING_FLOAT QM_CubicEaseOut( EASING_FLOAT p ) {
+	EASING_FLOAT f = ( p - 1 );
+	return f * f * f + 1;
+}
+/**
+*	@brief	Modeled after the piecewise cubic
+*			y = (1/2)((2x)^3)       ; [0, 0.5)
+*			y = (1/2)((2x-2)^3 + 2) ; [0.5, 1]
+**/
+static inline const EASING_FLOAT QM_CubicEaseInOut( EASING_FLOAT p ) {
+	if ( p < 0.5 ) {
+		return 4 * p * p * p;
+	} else {
+		EASING_FLOAT f = ( ( 2 * p ) - 2 );
+		return 0.5 * f * f * f + 1;
+	}
+}
+
+/**
+*	@brief	Modeled after the quartic x^4
+**/
+static inline const EASING_FLOAT QM_QuarticEaseIn( EASING_FLOAT p ) {
+	return p * p * p * p;
+}
+/**
+*	@brief	Modeled after the quartic y = 1 - (x - 1)^4
+**/
+static inline const EASING_FLOAT QM_QuarticEaseOut( EASING_FLOAT p ) {
+	EASING_FLOAT f = ( p - 1 );
+	return f * f * f * ( 1 - p ) + 1;
+}
+/**
+*	@brief	Modeled after the piecewise quartic
+*			y = (1/2)((2x)^4)        ; [0, 0.5)
+*			y = -(1/2)((2x-2)^4 - 2) ; [0.5, 1]
+**/
+static inline const EASING_FLOAT QM_QuarticEaseInOut( EASING_FLOAT p ) {
+	if ( p < 0.5 ) {
+		return 8 * p * p * p * p;
+	} else {
+		EASING_FLOAT f = ( p - 1 );
+		return -8 * f * f * f * f + 1;
+	}
+}
+
+/**
+*	@brief	Modeled after the quintic y = x^5
+**/
+static inline const EASING_FLOAT QM_QuinticEaseIn( EASING_FLOAT p ) {
+	return p * p * p * p * p;
+}
+/**
+*	@brief	Modeled after the quintic y = (x - 1)^5 + 1
+**/
+static inline const EASING_FLOAT QM_QuinticEaseOut( EASING_FLOAT p ) {
+	EASING_FLOAT f = ( p - 1 );
+	return f * f * f * f * f + 1;
+}
+/**
+*	@brief	Modeled after the piecewise quintic
+*			y = (1/2)((2x)^5)       ; [0, 0.5)
+*			y = (1/2)((2x-2)^5 + 2) ; [0.5, 1]
+**/
+static inline const EASING_FLOAT QM_QuinticEaseInOut( EASING_FLOAT p ) {
+	if ( p < 0.5 ) {
+		return 16 * p * p * p * p * p;
+	} else {
+		EASING_FLOAT f = ( ( 2 * p ) - 2 );
+		return  0.5 * f * f * f * f * f + 1;
+	}
+}
+
+/**
+*	@brief	Modeled after quarter-cycle of sine wave
+**/
+static inline const EASING_FLOAT QM_SineEaseIn( EASING_FLOAT p ) {
+	return sin( ( p - 1 ) * M_PI_2 ) + 1;
+}
+/**
+*	@brief	Modeled after quarter-cycle of sine wave (different phase)
+**/
+static inline const EASING_FLOAT QM_SineEaseOut( EASING_FLOAT p ) {
+	return sin( p * M_PI_2 );
+}
+/**
+*	@brief	Modeled after half sine wave
+**/
+static inline const EASING_FLOAT QM_SineEaseInOut( EASING_FLOAT p ) {
+	return 0.5 * ( 1 - cos( p * M_PI ) );
+}
+
+/**
+*	@brief	Modeled after shifted quadrant IV of unit circle
+**/
+static inline const EASING_FLOAT QM_CircularEaseIn( EASING_FLOAT p ) {
+	return 1 - sqrt( 1 - ( p * p ) );
+}
+/**
+*	@brief	Modeled after shifted quadrant II of unit circle
+**/
+static inline const EASING_FLOAT QM_CircularEaseOut( EASING_FLOAT p ) {
+	return sqrt( ( 2 - p ) * p );
+}
+/**
+*	@brief	Modeled after the piecewise circular function
+*			y = (1/2)(1 - sqrt(1 - 4x^2))           ; [0, 0.5)
+*			y = (1/2)(sqrt(-(2x - 3)*(2x - 1)) + 1) ; [0.5, 1]
+**/
+static inline const EASING_FLOAT QM_CircularEaseInOut( EASING_FLOAT p ) {
+	if ( p < 0.5 ) {
+		return 0.5 * ( 1 - sqrt( 1 - 4 * ( p * p ) ) );
+	} else {
+		return 0.5 * ( sqrt( -( ( 2 * p ) - 3 ) * ( ( 2 * p ) - 1 ) ) + 1 );
+	}
+}
+
+/**
+*	@brief	Modeled after the exponential function y = 2^(10(x - 1))
+**/
+static inline const EASING_FLOAT QM_ExponentialEaseIn( EASING_FLOAT p ) {
+	return ( p == 0.0 ) ? p : pow( 2, 10 * ( p - 1 ) );
+}
+/**
+*	@brief	Modeled after the exponential function y = -2^(-10x) + 1
+**/
+static inline const EASING_FLOAT QM_ExponentialEaseOut( EASING_FLOAT p ) {
+	return ( p == 1.0 ) ? p : 1 - pow( 2, -10 * p );
+}
+/**
+*	@brief	Modeled after the piecewise exponential
+*			y = (1/2)2^(10(2x - 1))         ; [0,0.5)
+*			y = -(1/2)*2^(-10(2x - 1))) + 1 ; [0.5,1]
+**/
+static inline const EASING_FLOAT QM_ExponentialEaseInOut( EASING_FLOAT p ) {
+	if ( p == 0.0 || p == 1.0 ) return p;
+
+	if ( p < 0.5 ) {
+		return 0.5 * pow( 2, ( 20 * p ) - 10 );
+	} else {
+		return -0.5 * pow( 2, ( -20 * p ) + 10 ) + 1;
+	}
+}
+
+/**
+*	@brief	Modeled after the damped sine wave y = sin(13pi/2*x)*pow(2, 10 * (x - 1))
+**/
+static inline const EASING_FLOAT QM_ElasticEaseIn( EASING_FLOAT p ) {
+	return sin( 13 * M_PI_2 * p ) * pow( 2, 10 * ( p - 1 ) );
+}
+/**
+*	@brief	Modeled after the damped sine wave y = sin(-13pi/2*(x + 1))*pow(2, -10x) + 1
+**/
+static inline const EASING_FLOAT QM_ElasticEaseOut( EASING_FLOAT p ) {
+	return sin( -13 * M_PI_2 * ( p + 1 ) ) * pow( 2, -10 * p ) + 1;
+}
+/**
+*	@brief	Modeled after the piecewise exponentially-damped sine wave:
+*			y = (1/2)*sin(13pi/2*(2*x))*pow(2, 10 * ((2*x) - 1))      ; [0,0.5)
+*			y = (1/2)*(sin(-13pi/2*((2x-1)+1))*pow(2,-10(2*x-1)) + 2) ; [0.5, 1]
+**/
+static inline const EASING_FLOAT QM_ElasticEaseInOut( EASING_FLOAT p ) {
+	if ( p < 0.5 ) {
+		return 0.5 * sin( 13 * M_PI_2 * ( 2 * p ) ) * pow( 2, 10 * ( ( 2 * p ) - 1 ) );
+	} else {
+		return 0.5 * ( sin( -13 * M_PI_2 * ( ( 2 * p - 1 ) + 1 ) ) * pow( 2, -10 * ( 2 * p - 1 ) ) + 2 );
+	}
+}
+
+/**
+*	@brief	Modeled after overshooting cubic y = 1-((1-x)^3-(1-x)*sin((1-x)*pi))
+**/
+static inline const EASING_FLOAT QM_BackEaseIn( EASING_FLOAT p ) {
+	return p * p * p - p * sin( p * M_PI );
+}
+/**
+*	@brief	Modeled after overshooting cubic y = 1-((1-x)^3-(1-x)*sin((1-x)*pi))
+**/
+static inline const EASING_FLOAT QM_BackEaseOut( EASING_FLOAT p ) {
+	EASING_FLOAT f = ( 1 - p );
+	return 1 - ( f * f * f - f * sin( f * M_PI ) );
+}
+/**
+*	@brief	Modeled after the piecewise overshooting cubic function:
+*			y = (1/2)*((2x)^3-(2x)*sin(2*x*pi))           ; [0, 0.5)
+*			y = (1/2)*(1-((1-x)^3-(1-x)*sin((1-x)*pi))+1) ; [0.5, 1]
+**/
+static inline const EASING_FLOAT QM_BackEaseInOut( EASING_FLOAT p ) {
+	if ( p < 0.5 ) {
+		EASING_FLOAT f = 2 * p;
+		return 0.5 * ( f * f * f - f * sin( f * M_PI ) );
+	} else {
+		EASING_FLOAT f = ( 1 - ( 2 * p - 1 ) );
+		return 0.5 * ( 1 - ( f * f * f - f * sin( f * M_PI ) ) ) + 0.5;
+	}
+}
+
+static inline const EASING_FLOAT QM_BounceEaseOut( EASING_FLOAT p );
+/**
+*	@brief
+**/
+static inline const EASING_FLOAT QM_BounceEaseIn( EASING_FLOAT p ) {
+	return 1 - QM_BounceEaseOut( 1 - p );
+}
+/**
+*	@brief
+**/
+static inline const EASING_FLOAT QM_BounceEaseOut( EASING_FLOAT p ) {
+	if ( p < 4 / 11.0 ) {
+		return ( 121 * p * p ) / 16.0;
+	} else if ( p < 8 / 11.0 ) {
+		return ( 363 / 40.0 * p * p ) - ( 99 / 10.0 * p ) + 17 / 5.0;
+	} else if ( p < 9 / 10.0 ) {
+		return ( 4356 / 361.0 * p * p ) - ( 35442 / 1805.0 * p ) + 16061 / 1805.0;
+	} else {
+		return ( 54 / 5.0 * p * p ) - ( 513 / 25.0 * p ) + 268 / 25.0;
+	}
+}
+/**
+*	@brief
+**/
+static inline const EASING_FLOAT QM_BounceEaseInOut( EASING_FLOAT p ) {
+	if ( p < 0.5 ) {
+		return 0.5 * QM_BounceEaseIn( p * 2 );
+	} else {
+		return 0.5 * QM_BounceEaseOut( p * 2 - 1 ) + 0.5;
+	}
+}
+// Undefine it again, prevent clutter.
+#undef EASING_FLOAT
